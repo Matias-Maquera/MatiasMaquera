fa1953c7ae86d4f1d3c62f9b9ee7cea8123d3e63
Matias-Maquera <matiasmaquera@gmail.com>
2023-11-28 15:48:22 -0400

    post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,41 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { Test1Component } from './test1/test1.component';
 
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LabsComponent } from './pages/labs/labs.component';
+import { HeaderComponent } from './commons/header/header.component';
+
+const config = {
+  apiKey: "AIzaSyDmQW0y94kM2f9s8s-YHIa5FCLlc1v47KY",
+  authDomain: "apirest-bc083.firebaseapp.com",
+  databaseURL: "https://apirest-bc083-default-rtdb.firebaseio.com",
+  projectId: "apirest-bc083",
+  storageBucket: "apirest-bc083.appspot.com",
+  messagingSenderId: "263020042066",
+  appId: "1:263020042066:web:31eee5662a14fcede80c8b",
+  measurementId: "G-9L7JJLK4SX"
+};
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     Test1Component,
+    RegisterComponent,
+    LoginComponent,
+    LabsComponent,
+    HeaderComponent,
     
   ],
   imports: [
     BrowserModule,
+    provideFirebaseApp(() => initializeApp(config)),
+    provideAuth(() => getAuth()), 
     AppRoutingModule,
     HttpClientModule,
     FormsModule
